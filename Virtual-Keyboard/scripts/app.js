8b0f211c90972f927c246e4f65a3cd04c230383b
Pernell Styles Jr <82741858+Stylesp007@users.noreply.github.com>
2023-02-21 19:42:03 -0500

    changes to the fetch

--- Virtual-Keyboard/scripts/app.js
+++ Virtual-Keyboard/scripts/app.js
@@ -5,18 +5,11 @@ const spaceBtn = document.querySelector('.space-button')
 // CREATE AND BIND A BTN THAT SEARCHS FOR THIS POKEMON
 // GET A SHORT DESCRIPTION OF THE POKEMON AND ITS ABILITYS
 // use a counter to increment the moves in the pokemons data
-spaceBtn.addEventListener('click', () => {
-  fetch(`https://pokeapi.co/api/v2/pokemon/`+USERINPUT.value)
-    .then(response => response.json())
-    .then(data => {
-      
-      // const ability = data['abilities'][1]['ability']['name'];
-      // const height = `${data['height']}'0`;
-      // const weight = `${data['weight']} lbs`;
-      // console.log(ability, height, weight);
-      console.log(pokeMoves);
-      console.log(data);
-    }).catch((err) => {
-      console.log('Pokemon not found', err);
-    })
-});
+async function pokemonSearch () {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/`+USERINPUT.value);
+  const pokemon = res.json();
+  console.log(pokemon);
+}
+
+
+spaceBtn.addEventListener('click', pokemonSearch);
