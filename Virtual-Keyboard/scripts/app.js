55c1598975e55400c0bccd1a9c558be934815803
Pernell Styles Jr <82741858+Stylesp007@users.noreply.github.com>
2023-02-23 11:18:09 -0500

    created a object

--- Virtual-Keyboard/scripts/app.js
+++ Virtual-Keyboard/scripts/app.js
@@ -1,15 +1,36 @@
-const searchPokemon = document.getElementById('search-box');
-const USERINPUT = document.querySelector('.inputValue');
-const spaceBtn = document.querySelector('.space-button')
 
-// CREATE AND BIND A BTN THAT SEARCHS FOR THIS POKEMON
-// GET A SHORT DESCRIPTION OF THE POKEMON AND ITS ABILITYS
-// use a counter to increment the moves in the pokemons data
-async function pokemonSearch () {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/`+USERINPUT.value);
-  const pokemon = res.json();
-  console.log(pokemon);
-}
+const pokeSearch = document.getElementById('search-pokemon');
 
 
-spaceBtn.addEventListener('click', pokemonSearch);
+const pokemonHandler = () => {
+  const pokeName = document.getElementById('pokeName').value;
+  const pokeAbility = document.getElementById('pokeAbility').value;// return a boolan value if the pokemon has the ability and return top five abilities. 
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`).then(response => response.json()).then((data) => {
+    const pokeObj = {
+      pokemonName:[data.name],
+      pokemonHeight:[data.height],
+      pokemonWeight:[data.weight]
+      // pokemmonImg:[data.sprites.official-artwork.front_default],
+
+    //   render() {
+    //   const pokeEl = document.createElement('img');
+    //   const pokeImg = data.sprites.front_default;
+    // // console.log(data);
+
+    //   pokeEl.innerHTML = `
+    //   <div>
+    //     <img src="${pokeImg}" alt = "${data.name}"/>
+    //   </div>`;
+    //   }
+    }
+    console.log(pokeObj.pokemonName);
+    console.log(pokeObj.pokemonHeight);
+    console.log(pokeObj.pokemonWeight);
+    
+  })
+  
+  
+};
+
+pokeSearch.addEventListener('click', pokemonHandler);
